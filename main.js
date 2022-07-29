4a592eef0c8bd4d87cc81e1f27cc2985c17a8389
Igor Gabriel <106640138+IgorGJN@users.noreply.github.com>
2022-07-29 08:23:58 -0300

    CRUD
    CRUD funcionando, sem vinculação com o layout

--- main.js
+++ main.js
@@ -8,6 +8,50 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
     
+
+const tempClient = {
+    nome: "Fernando",
+    email: "igor@github.com.br",
+    celular: "73982238896",
+    cidade: "itanhem"
+}
+
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [];
+const setLocalStorage =(dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient));
+
+
+// CRUD - creat read update delete
+
+// CRUD - READ
+const readClient = () => getLocalStorage();
+
+// CRUD - CREATE
+const createClient = (client) => {
+    const dbClient = getLocalStorage();
+    dbClient.push (client)
+    setLocalStorage(dbClient)
+}
+
+// CRUD - UPDATE
+const updateClient = (index, client) => {
+    const dbClient = readClient()
+    dbClient[index] = client
+    setLocalStorage(dbClient)
+}
+
+// CRUD - DELETE
+const deleteClient = (index) => {
+    const dbClient = readClient()
+    dbClient.splice(index, 1)
+    setLocalStorage(dbClient)
+}
+
+
+
+
+// Eventos
+
 document.getElementById('cadastrarIF')
     .addEventListener('click', openModal)
 
