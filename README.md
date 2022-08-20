eab5997ff1b42d89c84d93eb2e34f288c285e477
Igor Gabriel <106640138+IgorGJN@users.noreply.github.com>
2022-08-20 11:51:07 -0300

    Update JS
    Concluido

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # crud-codigos-if
- Um mini projeto Java Script
+
+## 💻 Sobre o Projeto
+Um mini projeto Java Script
+
+
+## 🔗 Como acessar
+Link -> [https://igorgjn.github.io/crud-codigos-if/](https://igorgjn.github.io/crud-codigos-if/)
+
+### 📅 Ultima atualização 
+20 de Agosto de 2022
+
+## 🎨 Layout
+<code><img height="300px" src="layout.gif" alt="Layout do site"/></code>
+<!--
+## 🆕 Novidades
+Layout responsivo para mobile. 
+Organização dos arquivos. -->
+
+<!-- ### Proximas atualizações 
+## 🔢 Versões
+-->
+## 🛠 Tecnologias
+
+As seguintes ferramentas foram usadas na construção do projeto:
+
+<code><img height="32" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg" alt="JS"/></code>
+<code><img height="32" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" alt="HTML5"/></code>
+<code><img height="32" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" alt="CSS"/></code>
+
+## 📝 Licença
+
+Este projeto esta sobe a licença MIT.
+
+Feito por Igor Gabriel - [Entre em contato!](https://www.linkedin.com/in/IgorGJN/)
+
+&copy; Copyright I Dev - 2022
--- main.js
+++ main.js
@@ -5,14 +5,16 @@
 const openModal = () => document.getElementById('modal')
     .classList.add('active')
 
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
+const closeModal = () => {
+    clearFields()
+    document.getElementById('modal').classList.remove('active')
+}
     
 
 const tempClient = {
     nome: "Fernando",
     email: "igor@github.com.br",
-    celular: "73982238896",
+    telefone: "73982238896",
     cidade: "itanhem"
 }
 
@@ -47,8 +49,103 @@ const deleteClient = (index) => {
     setLocalStorage(dbClient)
 }
 
+// Verificação dos clientes
+const isValidFields = () => {
+    return document.getElementById('form').reportValidity()
+}
 
+// Limpa campos
 
+const clearFields = () => {
+    const fields = document.querySelectorAll('.modal-field')
+    fields.forEach(field => field.value = "")
+}
+
+// Interação com o layout
+
+const saveClient = () => {
+    if (isValidFields()) {
+        const client = {
+            nome: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            telefone: document.getElementById('telefone').value,
+            cidade: document.getElementById('cidade').value,
+        }
+        const index = document.getElementById('nome').dataset.index
+        if (index == "new") {
+            createClient(client);
+            updateTable();
+            closeModal();
+        } else {
+            updateClient(index, client)
+            updateTable();
+            closeModal();
+        }
+    }
+}
+
+// Atualização da tela
+
+const createRow = (client, index) => {
+    const newRow = document.createElement('tr')
+    newRow.innerHTML = `
+    <td>${client.nome}</td>
+    <td>${client.email}</td>
+    <td>${client.telefone}</td>
+    <td>${client.cidade}</td>
+    <td>
+        <button type="button" class="button green" id="edit-${index}" data-action="edit">editar</button>
+        <button type="button" class="button red" id="delete-${index}" data-action="delete">excluir</button>
+    </td>
+    `
+    document.querySelector('#tabela-cliente>tbody').appendChild(newRow);
+}
+
+const clearTable = () => {
+    const rows = document.querySelectorAll('#tabela-cliente>tbody tr')
+    rows.forEach(row => row.parentNode.removeChild(row));
+}
+
+const updateTable = () => {
+    clearTable();
+    const dbClient = readClient();
+    dbClient.forEach(createRow);
+}
+
+updateTable();
+
+// Editar e excluir linhas
+
+const fillFields = (client) => {
+    document.getElementById('nome').value = client.nome;
+    document.getElementById('email').value = client.email;
+    document.getElementById('telefone').value = client.telefone;
+    document.getElementById('cidade').value = client.cidade;
+    document.getElementById('nome').dataset.index = client.index
+}
+
+const editClient = (index) => {
+    const client = readClient()[index]
+    client.index = index;
+    fillFields(client);   
+    openModal(); 
+}
+
+const editDelete = (event) => {
+    if (event.target.type == 'button') {
+        const [action, index] = event.target.id.split('-')
+        if (action == 'edit') {
+            editClient(index);
+        } else {
+            const client = readClient()[index]
+            const responde = confirm (`Deseja realmente excluir o cliente ${client.nome}`)
+            if (responde) {
+                deleteClient(index);
+                updateTable();
+            }
+        }
+    } 
+}
 
 // Eventos
 
@@ -57,3 +154,9 @@ document.getElementById('cadastrarIF')
 
 document.getElementById('modalClose')
     .addEventListener('click', closeModal)
+
+document.getElementById('salvar')
+    .addEventListener('click', saveClient)
+
+document.querySelector('#tabela-cliente>tbody')
+    .addEventListener('click', editDelete)
